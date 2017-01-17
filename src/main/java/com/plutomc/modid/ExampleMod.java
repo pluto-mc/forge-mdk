8c9cf53a42f08c09cf8c778e90e4d11729334343
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2017-01-18 09:07:05 +1100

    Change defaults and add settings.gradle
    Exclude XCF files from JARs.

--- src/main/java/com/plutomc/modid/ExampleMod.java
+++ src/main/java/com/plutomc/modid/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.plutomc.modid;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -8,13 +8,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "modid";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+
     }
 }
